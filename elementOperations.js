f67deeab33bc75d03e09f756ea4ccb19adef4d17
swietonp <swietonp@icloud.com>
2023-07-03 16:24:45 +0200

    function for getting the most right element xpath

--- elementOperations.js
+++ elementOperations.js
@@ -20,3 +20,24 @@ function getMostRightElement(elements) {
   
     return mostRightElement;
   }
+
+  // Function for choosing the most right element by xpath
+  function getMostRightElementByXpath(xpath) {
+    const xpathResult = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  
+    let mostRightElement = null;
+    let mostRightXPosition = -Infinity;
+  
+    for (let i = 0; i < xpathResult.snapshotLength; i++) {
+      const element = xpathResult.snapshotItem(i);
+      const elementRect = element.getBoundingClientRect();
+      const elementRightX = elementRect.right;
+  
+      if (elementRightX > mostRightXPosition) {
+        mostRightXPosition = elementRightX;
+        mostRightElement = element;
+      }
+    }
+  
+    return mostRightElement;
+  }
